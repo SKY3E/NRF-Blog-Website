e5791665721c035820db4a56cb4cbcbfa086762a
SKY3E <bessin.raphael@gmail.com>
2023-03-11 11:56:52 -0500

    Auth Edits - 3/11/23

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,41 @@
+import { googleAuthProvider, auth } from '../lib/firebase';
+import { signInWithPopup } from 'firebase/auth';
+
 export default function EnterPage({ }) {
-    return (
-        <main>
-            <h1>Sign In</h1>
-        </main>
-    )
+  const user = null;
+  const username = null;
+  
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+  return (
+      <main>
+          {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}
+      </main>
+  );
+}
+
+// Sign in with google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider)
+    .then((re) => {
+      console.log(re);
+    });
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src='/google.png' /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign out</button>;
+}
+
+function UsernameForm() {
+
 }
