043393b2a65dab97759cb9757841ac2793fa87aa
SKY3E <bessin.raphael@gmail.com>
2023-03-11 20:00:57 -0500

    Auth Context Edits - 3/11/23

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,14 @@ import '../styles/globals.css';
 import GetServerSideProps from 'next/app';
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <UserContext.Provider value={{ user:{}, username: 'jeff' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
--- pages/enter.js
+++ pages/enter.js
@@ -1,9 +1,10 @@
 import { googleAuthProvider, auth } from '../lib/firebase';
 import { signInWithPopup } from 'firebase/auth';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
 
 export default function EnterPage({ }) {
-  const user = null;
-  const username = null;
+  const { user, username } = useContext(UserContext)
   
   // 1. user signed out <SignInButton />
   // 2. user signed in, but missing username <UsernameForm />
